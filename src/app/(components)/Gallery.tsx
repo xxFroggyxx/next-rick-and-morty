b9ab61643b6bf653b4f828b542a0a775719d2e02
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2023-08-31 00:32:44 +0200

    add data change

--- src/app/(components)/Gallery.tsx
+++ src/app/(components)/Gallery.tsx
@@ -1,16 +1,39 @@
 'use client';
-import React from 'react';
+
+import React, { useState } from 'react';
 import { useRickAndMortyCharacters } from '@/hooks/useRickAndMortyCharacters';
 import { CharacterTypes } from '@/data/types';
 
 export const Gallery = () => {
-  const { data, isError, isLoading } = useRickAndMortyCharacters();
+  const [page, setPage] = useState(1);
+  const { data, isError, isLoading } = useRickAndMortyCharacters(page);
 
   if (isError) return <div>An error has occurred.</div>;
   if (isLoading) return <div>Loading...</div>;
 
   const { info, results } = data;
+
+  const handlePrevButton = () => {
+    if (page > 1) setPage((prev) => prev - 1);
+  };
+
+  const handleNextButton = () => {
+    if (page < info.pages) setPage((next) => next + 1);
+  };
+
   const characters = results.map(({ id, name }: CharacterTypes) => <li key={id}>{name}</li>);
 
-  return <ul>{characters}</ul>;
+  return (
+    <>
+      <div className="flex gap-4">
+        <button className="bg-red-600 p-2" onClick={handlePrevButton}>
+          Prev Page
+        </button>
+        <button className="bg-green-600 p-2" onClick={handleNextButton}>
+          Next Page
+        </button>
+      </div>
+      <ul className="text-center">{characters}</ul>
+    </>
+  );
 };
--- src/app/(hooks)/useRickAndMortyCharacters.tsx
+++ src/app/(hooks)/useRickAndMortyCharacters.tsx
@@ -1,8 +1,8 @@
 import useSWR from 'swr';
 
-export const useRickAndMortyCharacters = () => {
+export const useRickAndMortyCharacters = (page: number = 1) => {
   const fetcher = (url: string) => fetch(url).then((response) => response.json());
-  const { data, error, isLoading } = useSWR(`https://rickandmortyapi.com/api/character`, fetcher);
+  const { data, error, isLoading } = useSWR(`https://rickandmortyapi.com/api/character/?page=${page}`, fetcher);
 
   return { data, isError: error, isLoading };
 };
