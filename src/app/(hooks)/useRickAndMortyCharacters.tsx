613a5bff001faa5aed021875ac59cb94fdfbf01e
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2023-09-11 14:25:12 +0200

    fix layout shift

--- src/app/(hooks)/useRickAndMortyCharacters.tsx
+++ src/app/(hooks)/useRickAndMortyCharacters.tsx
@@ -4,5 +4,11 @@ export const useRickAndMortyCharacters = (page: number = 1) => {
   const fetcher = (url: string) => fetch(url).then((response) => response.json());
   const { data, error, isLoading } = useSWR(`https://rickandmortyapi.com/api/character/?page=${page}`, fetcher);
 
+  const nextPage = Math.min(page + 1, 42);
+  const { data: nextPageData } = useSWR(`https://rickandmortyapi.com/api/character/?page=${nextPage}`, fetcher, {
+    revalidateOnFocus: false,
+    shouldRetryOnError: false,
+  });
+
   return { data, isError: error, isLoading };
 };
