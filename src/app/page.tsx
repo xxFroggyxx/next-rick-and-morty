43e017767f4f7bd4be7b6153bcdf81f448a6aee8
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2023-09-18 15:01:50 +0200

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Gallery } from "@/components/Gallery";
+import Logo from "../../public/Rick-And-Morty-Logo.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <h1>Rick and Morty</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-6 lg:p-4">
+      <Image
+        src={Logo}
+        alt="Official logo of Rick and Morty"
+        height={225}
+        priority
+      />
       <Gallery />
     </main>
   );
